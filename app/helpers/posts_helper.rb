1596879819bfb01b1079667ecafe388bca42da14
Dipil <dipil.saud@sproutify.com>
2011-03-29 09:11:07 +0545

    errors resolved

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,7 @@ module PostsHelper
     if post.publish==true
       content_tag :h3 do
         [
-          link_to (" #{post.title}", post_path(post)),
+          link_to(" #{post.title}", post_path(post)),
           link_to_icon("show", post_path(post)),
           link_to_icon("edit", edit_post_path(post)),
           link_to_icon("destroy", post_path(post), :method => :delete, :confirm => "Do you really want to destroy?"),
--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,21 @@ ActiveRecord::Schema.define(:version => 20110325105219) do
     t.datetime "updated_at"
   end
 
+  create_table "blogs", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.string   "postedby"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.datetime "time"
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "subject"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", :force => true do |t|
     t.string   "commented_by"
     t.text     "comment_body"
